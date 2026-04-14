85ed7a09ff87819eb227132ac8b3b5085aa0107b
shay-way <smithshaina413@gmail.com>
2026-04-13 19:53:45 -0700

    first commit

---
+++ cleaner.py
@@ -0,0 +1,11 @@
+def clean_data(data):
+    """
+    TODO: Implement your "clean_heartrate_data" function from TLAB #1 & #2
+    within this module. Note that this code will be *slightly* different
+    from your original function.
+    """
+    clean_data = []
+    for row in data:
+          if row != "minutes\n":
+               clean_data.append(float(row.strip()))
+    return clean_data
